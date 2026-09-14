bce3efc880ef0daeb03b49d8827c0d296232eb83
raclark4844-pixel <raclark4844@gmail.com>
2026-09-14 18:18:29 -0400

    Remove crypto bitcoin route

--- src/routes/crypto.bitcoin.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/crypto/bitcoin")({
-  beforeLoad: () => {
-    throw redirect({ to: "/", replace: true });
-  },
-});
