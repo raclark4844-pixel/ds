70018ff27f1142a3a98baf2bf9d278ed5a226c14
raclark4844-pixel <raclark4844@gmail.com>
2026-09-14 18:18:33 -0400

    Remove crypto business route

--- src/routes/crypto.business.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/crypto/business")({
-  beforeLoad: () => {
-    throw redirect({ to: "/", replace: true });
-  },
-});
