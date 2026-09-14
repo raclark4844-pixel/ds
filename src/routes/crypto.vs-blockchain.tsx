5fa01cea4bb6af0fa81860d8c403433e02af9527
raclark4844-pixel <raclark4844@gmail.com>
2026-09-14 18:18:57 -0400

    Remove crypto comparison route

--- src/routes/crypto.vs-blockchain.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/crypto/vs-blockchain")({
-  beforeLoad: () => {
-    throw redirect({ to: "/", replace: true });
-  },
-});
