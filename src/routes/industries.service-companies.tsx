13835d1030117ec28f5f388ce20c06afeb882408
raclark4844-pixel <raclark4844@gmail.com>
2026-09-14 18:17:59 -0400

    Remove industry service companies page

--- src/routes/industries.service-companies.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { assertPublished } from "@/lib/publish";
-
-export const Route = createFileRoute("/industries/service-companies")({
-  beforeLoad: () => assertPublished("industries"),
-});
