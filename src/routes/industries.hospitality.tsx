552ab242ee393dbb057e0b12f618366b434ad0ef
raclark4844-pixel <raclark4844@gmail.com>
2026-09-14 18:17:47 -0400

    Remove industry hospitality page

--- src/routes/industries.hospitality.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { assertPublished } from "@/lib/publish";
-
-export const Route = createFileRoute("/industries/hospitality")({
-  beforeLoad: () => assertPublished("industries"),
-});
